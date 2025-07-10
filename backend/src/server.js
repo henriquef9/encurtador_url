f26eef946df7207e4403aa5c3e1c33d3b0f0fc90
Antônio Henrique Pacheco Costa <102102220+henriquef9@users.noreply.github.com>
2025-07-10 09:56:42 -0300

    Alterar porta do servidor para 80 no arquivo server.js e atualizar configuração do proxy_pass no nginx.conf

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
